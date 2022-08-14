c45a58c4a21cfc9126a43e0a57a9c0ea79319a67
unknown <nauvallyhafizh@gmail.com>
2022-08-14 13:03:59 +0700

    nammbah tabel

--- database/migrations/2022_08_13_154747_create_readers_table.php
+++ database/migrations/2022_08_13_154747_create_readers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('emai')->unique();
             $table->string('password');
             $table->timestamps();
         });
